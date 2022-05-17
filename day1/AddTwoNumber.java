8994815b6ce720c22b6c919c3be42ec2bf9455d6
Rutuja Shinde <rutusantosh1234@gmail.com>
2022-05-17 08:19:22 +0000

    my first java prg

---
+++ day1/AddTwoNumber.java
@@ -0,0 +1,22 @@
+package com.demo;
+
+class Addtwonumbers {
+	
+	void Sumoftwonumbers() 
+	{
+		int number1 = 30;
+		int number2 = 30;
+        int result = number1 + number2;
+        
+        System.out.print("Sum of two numbers" + result);
+		
+	}
+}
+public class AddTwoNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Addtwonumbers object = new Addtwonumbers();
+        object.Sumoftwonumbers();
+}
+}
